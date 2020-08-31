89b15475c4e3f01906f58c79ca0f0a0b48b8de15
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-31 15:08:19 +0200

    Merge develop into adding-devise

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2020_08_31_121108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 5, scale: 2
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,12 +36,4 @@ ActiveRecord::Schema.define(version: 2020_08_31_121108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.decimal "price", precision: 5, scale: 2
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
