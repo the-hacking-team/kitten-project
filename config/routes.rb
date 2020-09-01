209f9d6391b02a1364f6da6bddbf0edcfbf38bfb
Laurent Saives <etrenjeu@gmail.com>
2020-09-01 21:34:11 +0200

    First stripe set-up for testing on production environment

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'items#index'
   
   resources :items, only: [ :index, :show]
+  resources :charges, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
