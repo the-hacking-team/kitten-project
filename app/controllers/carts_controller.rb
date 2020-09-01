f33882135efac45080d2d23ea6773ba8ab9b24c2
Mathieu Voland <mathieu.voland@gmail.com>
2020-09-02 01:49:29 +0200

    Cart table

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,20 +1,9 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
 
-  # Find the Cart whose User ID is current_user.id
-  # Get Cart Params Id
   def show
     @cart = Cart.find(params[:id])
     check_cart_belongs_to_current_user
-    @items = @cart.items
-    @user = current_user
-
-    @cart_items = CartItem.where(cart_id: @cart.id)
-
-    @total = 0
-    @cart_items.each do |cart_item|
-      @total += cart_item.item.price
-    end
   end
 
   def update
@@ -22,28 +11,27 @@ class CartsController < ApplicationController
     check_cart_belongs_to_current_user
     @item = Item.find(params[:cart][:item_id])
     if @cart.items.include?(@item)
-      flash[:notice] = 'Déjà dans le panier'
+      flash[:alert] = 'Déjà dans le panier'
     else
-      flash[:notice] = 'Bien ajouté au panier'
-      CartItem.create(cart_id: @cart.id, item_id: @item.id)
+      flash[:notice] = 'Bien ajouté au panier' if CartItem.create(cart_id: @cart.id, item_id: @item.id)
     end
     redirect_to request.referrer
   end
 
   def destroy
-    @cart = Cart.where(user_id: current_user.id).last
-    @cart_item2 = CartItem.find_by(cart_id: @cart.id, item_id: params[:item_id])
-    @cart_item2.destroy
+    @cart = Cart.find(params[:id])
+    check_cart_belongs_to_current_user
+    @cart_item = CartItem.find_by(cart_id: @cart.id, item_id: params[:item_id])
+    flash[:notice] = 'Bien supprimé panier' if @cart_item.destroy
 
-    puts params[:item_id]
-    redirect_to cart_path(params[:id])
+    redirect_to request.referrer
   end
 
   private
 
   def check_cart_belongs_to_current_user
     if @cart.user != current_user
-      flash[:alert] = "Panier d'un autre utilisateur"
+      flash[:alert] = 'Accès interdit'
       redirect_to root_path
     end
   end
