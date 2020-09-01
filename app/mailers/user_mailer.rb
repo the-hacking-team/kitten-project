20e0a25981526c64be19dda6c6fc085c52794807
Laurent Saives <etrenjeu@gmail.com>
2020-09-01 12:34:58 +0200

    deleting comments in French and modifying the url in welcome_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: ENV['SENDGRID_FROM']
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    # Setting @user to use it in the welcome_email view 
     @user = user 
-  #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    # We set the @url variable by getting the environment variable (url of our website)
     @url  = ENV['SENDGRID_HOSTNAME']
   
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
