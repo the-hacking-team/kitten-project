46bc893789126638d213061111075d92588d77d8
Laurent Saives <etrenjeu@gmail.com>
2020-09-02 16:26:41 +0200

    First email and view to sum-up delivery order to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,22 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
+  def order_delivery_email(order)
+    @order = order
+    puts "="*80
+    puts @order.inspect
+    puts "="*80
+    @user = @order.user
+    @url  = ENV['SENDGRID_HOSTNAME']
+
+    mail(to: @user.email, subject: 'Voici votre commande') 
+  end
+
+#  def order_delivery_email(user)
+#    @user = user
+#    @order = @user.orders.last
+#    @url  = ENV['SENDGRID_HOSTNAME']
+#
+#    mail(to: @user.email, subject: 'Voici votre commande') 
+#  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+
+  after_create :order_delivery
+
+
+  private
+
+  def order_delivery
+    UserMailer.order_delivery_email(self).deliver_now
+  end
+
 end
