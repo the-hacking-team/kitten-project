c73d53900cb808e0c8e0ef8377ed90a1e8f4f11e
Laurent Saives <etrenjeu@gmail.com>
2020-09-03 17:09:41 +0200

    Sending an email to administrators after a purchase

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,6 @@
 class ChargesController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
-  def index
-    # Warning : add this in the routes file if you want to use it
-  end
-
   def new
     # Warning : add this in the routes file if you want to use it
   end
@@ -15,7 +11,6 @@ class ChargesController < ApplicationController
     @order_amount = @user.cart.total_price
     # Amount in cents
     @amount_stripe = (@order_amount * 100).to_i
-    puts "#{@user.email} vous cherchez à payer votre commande qui coute #{@order_amount} €"
 
     # A Stripe object Customer is created : custumer.id is generated and can be re-used for the same customer
     customer = Stripe::Customer.create({
@@ -31,19 +26,34 @@ class ChargesController < ApplicationController
                                    })
 
     # Order creation
+    # ---------------
     @order = Order.create(user_id: @user.id)
     @cart.items.each do |item|
       OrderItem.create(order_id: @order.id, item_id: item.id)
     end
 
+    # Deliver the order to the user
+    # ------------------------------
     # See https://stackoverflow.com/questions/8709984/how-to-catch-error-exception-in-actionmailer
     begin
-      UserMailer.with(order: @order).order_delivery_email.deliver_now
+      UserMailer.with(order: @order, order_amount: @order_amount).order_delivery_email.deliver_now
     rescue Exception => e
       puts 'ERROR: order_delivery_email'
       # FIXME: can't see notice
       flash[:notice] = "Il y a eu un problème dans l'envoi du mail, merci de nous contacter"
     end
+
+    # Sending an email to each administrator
+    # --------------------------------------
+    @administrators = Administrator.all
+    @administrators.each do |admin|
+      begin
+      AdministratorMailer.with(order: @order, administrator: admin, order_amount: @order_amount).sum_up_sale_email.deliver_now
+      rescue Exception => e
+        puts 'ERROR: sum_up_sale_email'
+      end
+    end
+
 
     # FIXME: check if necessary?
     # Destroy cart
---
+++ test/mailers/administrator_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdministratorMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
